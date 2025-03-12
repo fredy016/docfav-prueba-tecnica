d48e38045261af1645ed3837f628492a07f623ca
fredy016 <fredy016@outlook.com>
2025-03-11 22:35:06 -0600

    feat(UserEntity): add doctrine annotations

--- src/Domain/User/Entity/User.php
+++ src/Domain/User/Entity/User.php
@@ -2,17 +2,34 @@
 
 namespace Src\Domain\User\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use DateTimeImmutable;
+
 use Src\Domain\User\ValueObject\ID;
 use Src\Domain\User\ValueObject\Name;
 use Src\Domain\User\ValueObject\Email;
 use Src\Domain\User\ValueObject\Password;
 
+
+#[ORM\Entity]
+#[ORM\Table(name: "user")]
 final class User
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
     private ID $id;
+
+    #[ORM\Column(type: "string", length: 100)]
     private Name $name;
+
+    #[ORM\Column(type: "string", length:100, unique: true)]
     private Email $email;
+
+    #[ORM\Column(type: "string")]
     private Password $password;
+
+    #[ORM\Column(type: "datetime")]
     private \DateTimeImmutable $createdAt;
 
     public function __construct(
@@ -25,7 +42,7 @@ final class User
         $this->name = $name;
         $this->email = $email;
         $this->password = $password;
-        $this->createdAt = new \DateTimeImmutable();
+        $this->createdAt = new DateTimeImmutable();
     }
 
     public function getId(): ID
