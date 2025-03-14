d5ebcb0f6bfa2559edce31d11f406d636e408c5f
fredy016 <fredy016@outlook.com>
2025-03-14 00:26:59 -0600

    feat(RegisterUserController.php): add register user controller
    controller in change of using te use case that creates users

---
+++ src/Infrastructure/Controller/RegisterUserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Src\Controller;
+
+use Exception;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+use Src\Application\UseCase\RegisterUserUseCase;
+use Src\Application\DTO\RegisterUserRequest;
+use Src\Application\DTO\UserResponseDTO;
+
+
+class RegisterUserController
+{
+    private RegisterUserUseCase $registerUserUseCase;
+
+    public function __construct(RegisterUserUseCase $registerUserUseCase)
+    {
+        $this->registerUserUseCase = $registerUserUseCase;
+    }
+
+    public function register(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['name'], $data['email'], $data['password'])) {
+            return new JsonResponse(['error' => 'Datos incompletos'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        $dto = new RegisterUserRequest(
+            $data['name'],
+            $data['email'],
+            $data['password']
+        );
+
+        try {
+            $user = $this->registerUserUseCase->execute($dto);
+
+            $useResponse = new UserResponseDTO($user)->toArray();
+
+            return new JsonResponse($useResponse, JsonResponse::HTTP_CREATED);
+
+        } catch (Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage()], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
